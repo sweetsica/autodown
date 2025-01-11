ce0703963bbf665cc25df8ba96b91097a69ac70a
sweetsica <barondeepsnow@gmail.com>
2025-01-11 14:29:48 +0700

    fourth

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use WeStacks\TeleBot\TeleBot;
 
-class TelegramBotController extends Controller
+class TelegramController extends Controller
 {
     //+++++++++++++++++++++++++++++++++++++++
     private $bot;
