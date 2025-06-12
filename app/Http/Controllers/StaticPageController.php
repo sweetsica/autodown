f9d7b50977e4641e0d63600d471baa954fd2a5f2
Snow Deep <barondeepsnow@gmail.com>
2025-06-12 17:06:04 +0700

    [fix] forward routes

--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -12,7 +12,8 @@ class StaticPageController extends Controller
         $url_forward = 'https://medownloader.com/wp-content/themes/twentysixteen/modules/fbdl.php?url='.$request['url'].'&token=bjvkjkjkajkajakoqo1292318fdvsja';
         // dd($url_forward);
         $response = Http::get($url_forward);
-        return $response->body();
+        $url_hd = $response->json()['link']['Download Video HD'];
+        return $url_hd;
         // dd($response);
 
         // return redirect()->route('static.index');
